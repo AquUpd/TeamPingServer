72ca696355fdddd0d2604d16464f1d8572828b1d
AqUpd <aquupd@ya.ru>
2022-07-01 12:58:45 +0300

    create new methods

--- src/main/java/com/aqupd/teampingserver/ServerThreads.java
+++ src/main/java/com/aqupd/teampingserver/ServerThreads.java
@@ -20,7 +20,6 @@ public class ServerThreads {
 
   private final boolean debug;
   private String nickname;
-  private String partyname = "";
 
   public ServerThreads(Socket socket) {
     this.socket = socket;
@@ -39,6 +38,7 @@ public class ServerThreads {
       boolean license = false;
       boolean init = true;
       boolean writedata = false;
+      String partyname = "";
       long lastinteraction = 0;
       int step = 0;
 
@@ -98,41 +98,7 @@ public class ServerThreads {
 
             switch (data.get("datatype").getAsString()) {
               case "ping":
-                JsonObject ping = new JsonObject();
-                JsonArray clr = new JsonArray();
-                ping.add("datatype", new JsonPrimitive("ping"));
-
-                JsonArray blockpos = new JsonArray();
-                blockpos.add(data.get("bp").getAsJsonArray().get(0));
-                blockpos.add(data.get("bp").getAsJsonArray().get(1));
-                blockpos.add(data.get("bp").getAsJsonArray().get(2));
-                ping.add("bp", blockpos);
-                ping.add("type", data.get("type"));
-                ping.add("isEntity", data.get("isEntity"));
-
-                clr.add(randomcolor.getRed());
-                clr.add(randomcolor.getGreen());
-                clr.add(randomcolor.getBlue());
-
-                ping.add("color", clr);
-                ping.add("nickname", new JsonPrimitive(nickname));
-                ping.add("time", new JsonPrimitive(System.currentTimeMillis()));
-                ping.add("uuid", data.get("uuid"));
-
-                if(partyname.length() > 0){
-                  Map<String, Socket> conns = parties.get(partyname);
-                  Iterator<Map.Entry<String, Socket>> clientMap = conns.entrySet().iterator();
-                  while (clientMap.hasNext()) {
-                    Map.Entry<String, Socket> client = clientMap.next();
-                    try {
-                      PrintWriter swriter = new PrintWriter(client.getValue().getOutputStream(), true);
-                      swriter.println(ping);
-                    } catch (SocketException ex) {
-                      if (client.getValue().isClosed()) clientMap.remove();
-                    }
-                  }
-                }
-
+                generatePingJson(partyname, data);
                 lastinteraction = System.currentTimeMillis();
                 break;
               case "party":
@@ -146,16 +112,8 @@ public class ServerThreads {
                       local.put(nickname, socket);
                       parties.put(partyname, local);
                     }
-                    JsonObject jo = new JsonObject();
-                    JsonArray ja = new JsonArray();
-                    Map<String, Socket> conns = parties.get(partyname);
-                    for (String client : conns.keySet()) ja.add(client);
-                    jo.add("datatype", new JsonPrimitive("party"));
-                    jo.add("subtype", new JsonPrimitive("list"));
-                    jo.add("players", ja);
-
-                    writer.println(jo);
-                    LOGGER.info(parties.get(partyname).toString() + " " + jo);
+                    sendPlayerList(partyname);
+                    LOGGER.info(parties.get(partyname).toString());
                     break;
                   case "kick":
 
@@ -163,6 +121,8 @@ public class ServerThreads {
                   case "disconnect":
                     parties.get(partyname).remove(nickname);
                     if (parties.get(partyname).isEmpty()) parties.remove(partyname);
+                    else sendPlayerList(partyname);
+                    partyname = "";
                     break;
                 }
                 break;
@@ -181,6 +141,59 @@ public class ServerThreads {
           System.out.println(parties.toString());
         }
       }
+    }
+  }
+
+  private void sendDataToParty(String party, JsonObject data) {
+    Map<String, Socket> conns = parties.get(party);
+    Iterator<Map.Entry<String, Socket>> clientMap = conns.entrySet().iterator();
+    while (clientMap.hasNext()) {
+      Map.Entry<String, Socket> client = clientMap.next();
+      try {
+        PrintWriter swriter = new PrintWriter(client.getValue().getOutputStream(), true);
+        swriter.println(data);
+      } catch (IOException ex) {
+        if (client.getValue().isClosed()) clientMap.remove();
+      }
+    }
+  }
+
+  private void sendPlayerList(String party) {
+    JsonObject jo = new JsonObject();
+    JsonArray ja = new JsonArray();
+    Map<String, Socket> conns1 = parties.get(party);
+    for (String client : conns1.keySet()) ja.add(client);
+    jo.add("datatype", new JsonPrimitive("party"));
+    jo.add("subtype", new JsonPrimitive("list"));
+    jo.add("players", ja);
+
+    sendDataToParty(party, jo);
+  }
+
+  private void generatePingJson(String party, JsonObject jsonObject) {
+    JsonObject ping = new JsonObject();
+    JsonArray clr = new JsonArray();
+    ping.add("datatype", new JsonPrimitive("ping"));
+
+    JsonArray blockpos = new JsonArray();
+    blockpos.add(jsonObject.get("bp").getAsJsonArray().get(0));
+    blockpos.add(jsonObject.get("bp").getAsJsonArray().get(1));
+    blockpos.add(jsonObject.get("bp").getAsJsonArray().get(2));
+    ping.add("bp", blockpos);
+    ping.add("type", jsonObject.get("type"));
+    ping.add("isEntity", jsonObject.get("isEntity"));
+
+    clr.add(randomcolor.getRed());
+    clr.add(randomcolor.getGreen());
+    clr.add(randomcolor.getBlue());
+
+    ping.add("color", clr);
+    ping.add("nickname", new JsonPrimitive(nickname));
+    ping.add("time", new JsonPrimitive(System.currentTimeMillis()));
+    ping.add("uuid", jsonObject.get("uuid"));
+
+    if (party.length() > 0) {
+      sendDataToParty(party, ping);
     }
   }
 }
