575bdaf7a4e9003f0558e3065c6ca7817e150b6a
Ivan-Khar <aquupd@ya.ru>
2022-10-11 13:26:04 +0300

    rewrite time lol

--- src/main/java/com/aqupd/teampingserver/Main.java
+++ src/main/java/com/aqupd/teampingserver/Main.java
@@ -1,62 +1,26 @@
 package com.aqupd.teampingserver;
 
-import java.awt.*;
 import java.io.*;
 import java.net.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.List;
+
+import com.aqupd.teampingserver.utils.Config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("InfiniteLoopStatement")
 public class Main {
-  public static Map<String, LinkedHashMap<String, Socket>> parties = new HashMap<>();
-  public static SortedSet<String> publicList = new TreeSet<>();
-  public static Map<String, List<String>> banlist = new HashMap<>();
-  public static int playerCount = 0;
+
   public static final Logger LOGGER = LogManager.getLogger("TeamPing");
-  public static String version = "debug";
-  public static Long lastVersionCheck = 0L;
-  public static Color[] colors = new Color[] {
-    Color.decode("#f44336"),
-    Color.decode("#e81e63"),
-    Color.decode("#9c27b0"),
-    Color.decode("#673ab7"),
-    Color.decode("#3f51b5"),
-    Color.decode("#2196f3"),
-    Color.decode("#03a9f4"),
-    Color.decode("#00bcd4"),
-    Color.decode("#009688"),
-    Color.decode("#4caf50"),
-    Color.decode("#8bc34a"),
-    Color.decode("#cddc39"),
-    Color.decode("#ffeb3b"),
-    Color.decode("#ffc107"),
-    Color.decode("#ff9800"),
-    Color.decode("#ff5722")
-  };
 
   public static void main(String[] args) {
+    Config conf = Config.get;
+    conf.load();
 
-    try {
-      URL url = new URL("https://raw.githubusercontent.com/Ivan-Khar/TeamPing/master/CurrentVersion.txt");
-      Scanner s = new Scanner(url.openStream());
-      version = s.next();
-      lastVersionCheck = System.currentTimeMillis();
-    } catch(IOException ex) {
-      LOGGER.error("Couldn't get current version of the mod", ex);
-    }
-
-    try (ServerSocket serverSocket = new ServerSocket(28754)) {
+    try (ServerSocket serverSocket = new ServerSocket(conf.getPort(), 10, InetAddress.getByName(conf.getIp()))) {
       LOGGER.info("Server is listening on port " + serverSocket.getLocalPort());
       while (true) {
         Socket socket = serverSocket.accept();
-        String threadname = socket.getRemoteSocketAddress().toString();
-        LOGGER.info("New client connected! " + threadname.substring(1, threadname.indexOf(".")) + ".##" + threadname.substring(threadname.length()-9));
-        new ServerThreads(socket);
+        LOGGER.info("New client connected!");
       }
     } catch (IOException ex) {
       LOGGER.error("Server exception: ", ex);
---
+++ src/main/java/com/aqupd/teampingserver/utils/Config.java
@@ -0,0 +1,57 @@
+package com.aqupd.teampingserver.utils;
+
+import com.aqupd.teampingserver.Main;
+import com.google.gson.*;
+
+import java.io.*;
+
+public class Config {
+
+  private Config() {}                       //Create Instance
+  public static Config get = new Config();  //Of this class
+
+  private final File confFile = new File("./config.json");
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+  //All the data
+  private boolean debug = false;
+  private String ip = "0.0.0.0";
+  private int port = 28754;
+
+  //Getters
+  public String getIp() { return ip; }
+  public int getPort() { return port; }
+  public boolean isDebug() { return debug; }
+
+  //Config loader
+  public void load() {
+    if (!confFile.exists() || confFile.length() == 0) save();
+    try {
+      JsonObject data = gson.fromJson(new FileReader(confFile), JsonObject.class);
+      JsonElement setting;
+      if((setting = data.get("debug")) != null) debug = setting.getAsBoolean();
+      if((setting = data.get("ip")) != null) ip = setting.getAsString();
+      if((setting = data.get("port")) != null) port = setting.getAsInt();
+    } catch (FileNotFoundException ex) {
+      Main.LOGGER.trace("Couldn't load configuration file", ex);
+    }
+  }
+
+  //Config saver
+  public void save() {
+    try {
+      if (!confFile.exists()) { confFile.getParentFile().mkdirs(); confFile.createNewFile(); }
+
+      JsonObject jo = new JsonObject();
+      jo.add("debug", new JsonPrimitive(debug));
+      jo.add("ip", new JsonPrimitive(ip));
+      jo.add("port", new JsonPrimitive(port));
+
+      PrintWriter printwriter = new PrintWriter(new FileWriter(confFile));
+      printwriter.print(gson.toJson(jo));
+      printwriter.close();
+    } catch (IOException ex) {
+      Main.LOGGER.trace("Couldn't save configuration file", ex);
+    }
+  }
+}
